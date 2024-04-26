6eb70be1c0e31b5695775b65395395e0a782e3e9
Guilherme <guilhermesilvas2004@gmail.com>
2024-04-25 22:05:04 -0300

    Banco-golang

---
+++ Banco_SacarDepositarTranferir/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func main() {
+	contaDoGuilherme := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	contcontaDoGuilherme2 := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+
+	var contaDaCris *ContaCorrente
+	contaDaCris = new(ContaCorrente)
+	contaDaCris.titular = "Cris"
+	contaDaCris.saldo = 500
+
+	fmt.Println(contaDoGuilherme == contcontaDoGuilherme2)
+	fmt.Println(*contaDaCris)
+	fmt.Println(contaDoGuilherme)
+	fmt.Println(contaDaBruna)
+
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Sacar(400))
+	fmt.Println(contaDaSilvia.saldo)
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Google-Golang
+
+
+Na pasta Banco_SacarDepositarTransferir, eu criei um codigo que simula uma conta de Banco
